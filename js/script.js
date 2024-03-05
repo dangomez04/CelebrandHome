ba9f0626468c5b0c29b9c7ec2664b8e97e17b6d0
Daniel <danielgomezgarciaaa@gmail.com>
2024-03-05 21:11:10 +0100

    proyecto acabado, cambiado fuentes y añadido animacion de  figuras en la web

---
+++ js/script.js
@@ -0,0 +1,23 @@
+window.addEventListener('scroll', function() {
+    var donut = document.querySelector('.donut');
+    
+    //obtengo el valor del scroll actual, cada scroll equivale a 100
+    var scrollPosition = window.scrollY;
+    
+
+    // Ajustamos la posición del donut basándonos en la posición del scroll
+    donut.style.top = scrollPosition + 'px';
+    donut.style.right = scrollPosition + 'px';
+
+
+
+    //si el scroll vuelve a su posición inicial, que el donut vuelva a su pos inicial también
+
+    if (scrollPosition==0) {
+        donut.style.top = '0px';
+        donut.style.right = '-291px';
+
+    }
+    
+    
+});
