d7187f79cae505cd8cdad61b810207b069594db8
dangomez04 <danielgomezgarciaaa@gmail.com>
2024-03-06 09:16:52 +0100

    P.Acabado, añadido animaciones de transición de título y movimiento en la página de figuras flotantes

--- js/script.js
+++ js/script.js
@@ -2,15 +2,13 @@ window.addEventListener('scroll', function() {
     var donut = document.querySelector('.donut');
     
     //obtengo el valor del scroll actual, cada scroll equivale a 100
-    var scrollPosition = window.scrollY;
-    
+    var scrollPosition = window.scrollY/5;
 
     // Ajustamos la posición del donut basándonos en la posición del scroll
     donut.style.top = scrollPosition + 'px';
     donut.style.right = scrollPosition + 'px';
 
-
-
+   
     //si el scroll vuelve a su posición inicial, que el donut vuelva a su pos inicial también
 
     if (scrollPosition==0) {
